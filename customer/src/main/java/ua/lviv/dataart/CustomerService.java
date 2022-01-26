4897411aa1a6dbd19c27708a7298e69d61843ada
Teodor Hrabovenskyi <teodor.hrabovenskyi@dataart.com>
2022-01-26 16:58:40 +0200

    Writed two microservices, made communication between them with the help of RestTemplate

--- customer/src/main/java/ua/lviv/dataart/CustomerService.java
+++ customer/src/main/java/ua/lviv/dataart/CustomerService.java
@@ -1,9 +1,11 @@
 package ua.lviv.dataart;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public record CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate) {
+
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -12,6 +14,17 @@ public record CustomerService(CustomerRepository customerRepository) {
                 .build();
         // TODO: check if email valid
         // TODO: check if email not taken
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer); // Flush to get generated id
+
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        if (fraudCheckResponse == null || fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
+        // TODO: send notification
     }
 }
---
+++ customer/src/main/java/ua/lviv/dataart/FraudCheckResponse.java
@@ -0,0 +1,5 @@
+package ua.lviv.dataart;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+
+}
